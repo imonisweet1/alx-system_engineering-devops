c77cd3bea027258d35b9edd56cc40dbd5bc1b117
Isaac Imonivwerha <isaacimonivwerha@gmail.com>
2024-03-29 06:42:57 -0700

    made more corrections

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 #!/usr/bin/pup
 # Install fask version 2.1.0
-package {"flask":
-  ensure   => "2.1.0",
-  provider => "pip3",
-}
-# Install Werkzeug version 2.1.1
-package {'Werkzeug':
-  ensure   => "2.1.1",
-  provider => "pip3",
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
