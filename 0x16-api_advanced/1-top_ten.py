6c90b29cedb447a92f4e1d88bd25db81f4269b23
Isaac Imonivwerha <isaacimonivwerha@gmail.com>
2024-08-13 10:44:15 -0700

    a function that queries the Reddit API

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,36 +1,27 @@
 #!/usr/bin/python3
 """
-Function queries the Reddit API and prints
-the top ten hot posts of a subreddit
+Write a function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
 """
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """ Queries to Reddit API """
-    u_agent = 'Mozilla/5.0'
+    """
+    Function that returns the titles of the first 10 top hot
+    posts for a given subreddit
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
 
-    headers = {
-        'User-Agent': u_agent
-    }
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    user_agent = {"User-agent": 'Google Chrome Version 81.0.4044.129'}
+    param = {'limit': 10}
+    response = requests.get(url, headers=user_agent, params=param)
 
-    params = {
-        'limit': 10
-    }
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    res = requests.get(url,
-                       headers=headers,
-                       params=params,
-                       allow_redirects=False)
-    if res.status_code != 200:
-        print(None)
-        return
-    dic = res.json()
-    hot_posts = dic['data']['children']
-    if len(hot_posts) is 0:
-        print(None)
-    else:
-        for post in hot_posts:
-            print(post['data']['title'])
+    try:
+        res = response.json().get('data').get('children')
+        for i in res:
+            print(i.get('data')['title'])
+    except Exception:
+        print("None")
