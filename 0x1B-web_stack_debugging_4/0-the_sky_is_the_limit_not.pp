9281adf13d25c3423c3e038bda5d175cd0309313
isaac imonivwerha <isaacimonivwerha>
2024-08-22 11:14:40 +0000

    task 00

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,5 @@
 # remove nginx limit
-exec {'remove limit':
-    command => 'mv /etc/default/nginx /etc/default/nginx.bak; sudo service nginx restart',
-    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-    onlyif  => 'find /etc/default/nginx'
+exec {'modify max open file limit setting':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
